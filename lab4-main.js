d1df151d1874db369f58421be21e50d9919dabd2
Josh Clark <clark.2525@osu.edu>
2016-12-04 23:35:33 -0500

    asd

--- lab4-main.js
+++ lab4-main.js
@@ -1028,6 +1028,7 @@ function drawEnvironment() {
     mMatrix = popMatrix();
     pushMatrix(mMatrix);
     mMatrix = mat4.translate(mMatrix, [-1.0, 0.7, 0.0]);
+    mMatrix = mat4.rotate(mMatrix, degToRad(180), [0, 0, 1]);
     mMatrix = mat4.scale(mMatrix, [1.0, 0.75, 1.0]);
     //draw_sphere(colorEnum.GREEN, lightCoefEnum.LOW);
     draw_leaf_sphere();
